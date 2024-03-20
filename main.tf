0c7db28cc8b7f879d825884795dd538615a66981
mounika <mounikakopanathi1126@gmail.com>
2024-03-20 11:56:53 +0530

    terraform main

---
+++ main.tf
@@ -0,0 +1,10 @@
+resource "local_file" "my_pet" {
+  filename = var.filename
+  content  = var.content
+ 
+  }
+resource "random_pet" "petname" {
+  prefix    = "MR"
+  separator = "."
+  length    = "1"
+}
---
+++ variable.tf
@@ -0,0 +1,11 @@
+variable "filename" {
+    default = "birds.txt"
+    type = string
+    description = "this is the filename"
+}
+
+    variable "content" {
+    default ="I love birds"
+    description = "this is the content"
+
+}
